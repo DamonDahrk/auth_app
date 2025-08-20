7ffc5ae9b7a910f6d80fda47c2739f5930aa3cb8
Sayantan Chowdhury <shyuhaspivot@gmail.com>
2025-08-20 16:17:24 +0530

    adding the dbConfig and understand the importance of Naming conventions

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function SignUpPage() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Sign Up</h1>
+    </div>
+  )
+}
+
+ 
