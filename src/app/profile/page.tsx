5d0b2cf8a7fcad6164df881f4b1323e94b64567e
Sayantan Chowdhury <shyuhaspivot@gmail.com>
2025-08-26 00:13:18 +0530

    added user details on the profile page, and also used Link in the front end to redirect to a profile page on click of the button .

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,6 +5,8 @@ import React, {useState} from "react";
 import {toast} from "react-hot-toast";
 import {useRouter} from "next/navigation";
 
+//Link to go redirect to login page
+
 
 export default function ProfilePage() {
     const router = useRouter()
@@ -20,6 +22,12 @@ export default function ProfilePage() {
         }
     }
 
+    const getUserDetails = async () => {
+        const res = await axios.get('/api/users/me')
+        console.log(res.data);
+        setData(res.data.data._id)
+    }
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
@@ -32,6 +40,11 @@ export default function ProfilePage() {
         onClick={logout}
         className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >Logout</button>
+
+        <button
+        onClick={getUserDetails}
+        className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >GetUser Details</button>
 
 
             </div>
